e47261bf4e7ebf03b0e923f182fff6babf9c5e0f
Andre <andre_link27@web.de>
2018-07-15 16:03:50 +0200

    Add minor visual changes to border style

--- WebProject1/edit_account.php
+++ WebProject1/edit_account.php
@@ -4,19 +4,19 @@
             <table id="login" class="box">
                 <tr>
                     <td>Benutzername</td>
-                    <td><input name="USR" type="text" size="15" maxlength="100" required="required" value=<?php echo($_SESSION['user']['name'])?>></td>
+                    <td><input name="USR" type="text" size="15" maxlength="100" required="required" placeholder="Neuer Name"></td>
                 </tr>
                 <tr>
                     <td>Email</td>
-                    <td><input name="EML" type="email" size="15" maxlength="100" required="required" value=<?php echo($_SESSION['user']['email'])?>></td>
+                    <td><input name="EML" type="email" size="15" maxlength="100" required="required" placeholder="Neue Email"></td>
                 </tr>
                 <tr>
                     <td>Passwort</td>
-                    <td><input name="PWD" type="password" size="15" maxlength="100" required="required" value=<?php echo($_SESSION['user']['pwd'])?>></td>
+                    <td><input name="PWD" type="password" size="15" maxlength="100" required="required" placeholder="Neues Passwort"></td>
                 </tr>
                 <tr>
                     <td>Passwort wiederholen</td>
-                    <td><input name="PWW" type="password" size="15" maxlength="100" required="required" value=<?php echo($_SESSION['user']['pwd'])?>></td>
+                    <td><input name="PWW" type="password" size="15" maxlength="100" required="required" placeholder="Neues Passwort"></td>
                 </tr>
                 <td></td>
                 <td><input class="button" name="SUB" type="submit" value="Save Changes"></td>
